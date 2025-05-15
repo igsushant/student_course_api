b7095ad3d7186beb4fab5b79e14d3db842fa0410
igsushant <162614919+igsushant@users.noreply.github.com>
2025-05-15 16:02:51 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://student_user:sqlserver@localhost:5432/student_course_db"
+DATABASE_URL = "postgresql://username:passwordr@localhost:5432/database"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
